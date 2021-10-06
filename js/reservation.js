e08a4248ee10452fe47555b2483ff5ee463e24e7
lclucumi <luz.lucumi@correounivalle.edu.co>
2021-10-06 16:31:02 -0500

    reservationUpdate

--- js/reservation.js
+++ js/reservation.js
@@ -14,6 +14,7 @@ document
       () => {
         btn_cumple.value = "Reservar";
         alert("Reservado!");
+        localStorage.clear();
       },
       (err) => {
         btn_cumple.value = "Reservar";
